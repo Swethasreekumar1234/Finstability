8e0b931cd4e2819ac08cff1dafb56ce42446ba27
Swethasreekumar1234 <148451524+Swethasreekumar1234@users.noreply.github.com>
2026-04-02 10:44:22 +0530

    expo changes

---
+++ Finstability/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 20 }}>App is Working ✅</Text>
+    </View>
+  );
+}
